658b9756b1baa6b8cb9da9d92b90b45478599b7f
Florian Weber <florian.weber@ueberbit.de>
2025-09-14 19:45:48 +0200

    Add SPX

---
+++ Formula/php-spx.rb
@@ -0,0 +1,17 @@
+require_relative "../lib/php_pecl_formula"
+
+class PhpSpx < PhpPeclFormula
+  extension_dsl "SPX - A simple profiler for PHP"
+
+  url "https://github.com/NoiseByNorthwest/php-spx/archive/refs/tags/v0.4.20.tar.gz"
+  
+  on_macos do
+    configure_arg "--with-zlib-dir=#{MacOS.sdk_path_if_needed}/usr"
+  end
+
+  def install
+    configure_args << "--with-spx-assets-dir=#{share}/assets"
+    super
+  end
+
+end
