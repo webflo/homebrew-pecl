ec0c6dfc12c9e0599da7bb1d166a7c2b05778e8e
Florian Weber <florian@webflo.org>
2025-11-20 15:00:17 +0100

    Update Formula/php-spx.rb
    Co-authored-by: Kevin Abel <kevin.abel.0@gmail.com>

--- Formula/php-spx.rb
+++ Formula/php-spx.rb
@@ -6,7 +6,7 @@ class PhpSpx < PhpPeclFormula
   homepage "https://github.com/NoiseByNorthwest/php-spx"
   url "https://github.com/NoiseByNorthwest/php-spx/archive/refs/tags/v0.4.20.tar.gz"
   sha256 "8de7f8e6137667dbe7e92ba552ccb3b3b3745664efb9af88ece8bda0f58fc94f"
-  license "GPL-3.0"
+  license "GPL-3.0-or-later"
 
   on_macos do
     configure_arg "--with-zlib-dir=#{MacOS.sdk_path_if_needed}/usr"
